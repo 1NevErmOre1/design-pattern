645ebb9e1d41500bc0cb641457a7e5b30b588e52
â910246864@qq.com <910246864@qq.com>
2022-04-19 15:40:27 +0800

    Initial commit

---
+++ src/main/java/com/lighthouse/designpattern/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.lighthouse.designpattern.controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gjk
+ * @date 2022/4/19 15:34
+ */
+@Slf4j
+@RestController
+public class TestController {
+}
---
+++ src/test/java/com/lighthouse/designpattern/DesignPatternApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lighthouse.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(111);
+    }
+
+}
